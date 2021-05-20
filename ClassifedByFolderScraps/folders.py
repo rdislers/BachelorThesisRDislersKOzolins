13e664f5a27e4346e0815467c1bc3aa7bc7d1666
Renārs Dišlers <74921204+rdislers@users.noreply.github.com>
2021-05-20 12:05:38 +0300

    Delete ClassifedByFolderScraps directory

--- ClassifedByFolderScraps/folders.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-
-
-
-df = pd.read_excel('C:/Users/hokej/Desktop/ai-job-title-area-classification-master/ClassfiedFolders/JobDepartments.xlsx',engine='openpyxl')
-
-for index, row in df.iterrows():
-    os.mkdir(row["department"])
